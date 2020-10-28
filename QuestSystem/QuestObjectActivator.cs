b73fa2ad3e29ec55604e834b74c61d65d43494b0
CTY <cty@CTYde-MacBook-Pro.local>
2020-10-28 13:10:20 +0800

    quest system v0.5

---
+++ QuestSystem/QuestObjectActivator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObjectActivator : MonoBehaviour
+{
+	//public GameObject objectToActivate;
+
+	public string[] questToCheck;
+	bool initCheck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!initCheck) {
+        	initCheck = true;
+        	if(CheckActivation()) {
+        		doSomething();
+        	}
+        }
+    }
+
+    public bool CheckActivation() {
+    	bool activation = true;
+    	for(int i=0;i<questToCheck.Length;i++)
+	    	if(!QuestManager.instance.CheckIfComplete(questToCheck[i]))
+	    		activation = false;
+
+	    return activation;
+    }
+
+    public void doSomething() {
+    	
+    }
+}
